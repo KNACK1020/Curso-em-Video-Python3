ecf92a40f97414d7a62389aec8eedb52b0d2743f
KNACK1020 <dauamachado.com.br@gmail.com>
2022-12-19 22:32:40 -0300

    Ex feito e README modificado novamente
    Ex 023 terminado (embora diferente da solução proposta pelo canal, ainda funciona) e mais uma modificação no README

--- Ex_023.py
+++ Ex_023.py
@@ -1,6 +1,7 @@
-num = input('Digite um número inteiro de 1 até 9999: ')
+num = int(input('Digite um número inteiro de 1 até 9999: ')) + 1000# Lê o número e adiciona 1000 nele, para que tenha as 4 casas necessárias.
+strnum = str(num)# Trasforma o número em uma string para poder ser usado com os colchetes (se não me engano isso se chama idexação, ou algo assim).
 
-print(f'Unidade: {num[0::2]}')
-print(f'Dezena: {num}')
-print(f'Centena: {num}')
-print(f'Milhar: {num}')
+print(f'Unidade: {strnum[3]}')# O número tem 4 casas por conta dos 1000 adicionados acima,
+print(f'Dezena: {strnum[2]}')# então não existe problema caso o número digitado seja de menos de menos de 4 casas.
+print(f'Centena: {strnum[1]}')
+print(f'Milhar: {int(strnum[0]) - 1}')# trasforma a primeira casa do número em inteiro, e então subtrai 1 dele, para remover aqueles 1000 adicionados anteriormente.
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Curso em Vídeo Python
- Todos os exercícios do curso de Python do canal do Youtube "Curso em vídeo" resolvidos (ainda em desenvolvimento)
+Todos os exercícios do curso de Python do canal do Youtube "Curso em vídeo" resolvidos.
+Ainda em desenvolvimento, espere uma média de 1 atualização por semana.
